330c86e58ee5892d3b0e849ee98b1b0a2a7415c3
YaoTonq <yat529@gmail.com>
2018-01-30 15:47:39 -0500

    fix code issue

--- im.js
+++ im.js
@@ -10,40 +10,36 @@
         // -------------------- Core Functions --------------------------
         // 
         
-        extend: function(obj, name){
-            if(typeof obj !== "object" && typeof obj !== "function") return "Argument must be an Object or function!";
-            
-            if(typeof obj === "object"){
+        extend: function(obj){
+            try {
+                if(typeof obj !== "object") throw new Error("The arugument must be a object with methods");
                 for(var prop in obj){
-                    if(this[prop]) return "An object with same name already exists!";
+                    if(this[prop]) throw new Error("Method: '" + prop + "' already exists! Failure to extend.");
                     this[prop] = obj[prop];
                 }
-            }else if(typeof obj === "function"){
-                var name = name || obj[name];
-                if(!name) "Function cannot be anonymouse!";
-                this[name] = obj;
+                return this;
             }
-
-            return this;
+            catch(e) { console.error(e.name, e.message) }
         },
 
         // Selector API, can use .classname or #id
         get: function(selector, ctx){
-            var match = sReg.exec(selector),
-                ctx;
-
-            if(!match) return "No selector matches!";
+            var ctx,
+                match = sReg.exec(selector);
 
             ctx = (ctx && typeof ctx === "object")? ctx : document;
-
+            
             if(match[1]){
                 this[0] = ctx.getElementsByClassName(match[1]);
             }else if(match[2]){
                 this[0] = document.getElementById(match[2]);
-            }
+            } 
 
-            this.length = 1;
-            return this;
+            try {
+                if(!this[0]) throw new Error("No selector matches '" + selector +"'!");
+                this.length = 1;
+                return this;
+            } catch(e) { console.error(e.name, e.message) }
         },
 
         // log the object
@@ -52,52 +48,21 @@
         },
 
         // loop thru an Array
-        each: function(arr, callback){
-            var array,
-                fn;
+        each: function(callback){
+            for(let i = 0; i < this[0].length; i++) {
+                var obj = createObj( this[0][i] );
 
-            // check if arr is passed as 1st param
-            if(arguments.length === 2){
-                if(!arr.length && typeof callback !== "function") return "Parameter Type Error!";
-                array = arr;
-                fn = callback;
-            }
-
-            // if only one parameter passed in
-            if(arguments.length === 1){
-                // if only pass in array, but not callback function, return the array
-                // if(arr.length) return arr;
-                // if only pass in callback function
-                if(typeof arr === "function") fn = arr;
-                // if HTMLCollection array is exists, assign it to array
-                if(this[0]) array = this[0];
-            }
-
-            // now array & fn is ready, do something
-            for(var i = 0; i < array.length; i++){
-                // each iteration is an IIFE, so that this of the callback function 
-                // in each iteration is unique
-                (function(j, f){
-                    // New object created each time, so this will refer to this new obj
-                    var obj = Object.create(init);
-                    obj[0] = array[j];
-                    // change this and the argument to obj
-                    f.call(obj, j);
-                })(i, fn);
+                callback.call(obj, i);
             }
         },
 
         // Get first item in the array
         first: function(){
-            // this[0] = this[0][0];
-            // return this;
             return createObj( this[0][0] );
         },
 
         // Get last item in the array
         last: function(){
-            // this[0] = this[0][(this[0].length - 1)];
-            // return this;
             return createObj( this[0][(this[0].length - 1)] );
         },
 
@@ -311,43 +276,35 @@
         },
 
         data: function(attr){
-            if(typeof attr !== "string") return "Attributes must be String Type";
             return this[0].getAttribute("data-" + attr) || undefined;
         },
 
         // toggle classname of a DOM element
         toggleClass: function(cls){
-            if(typeof cls !== "string") return "Class name must be String Type";
             this[0].classList.toggle(cls);
             return this;
         },
 
         // add classname
         addClass: function(cls){
-            if(typeof cls !== "string") return "Class name must be String Type";
             this[0].classList.add(cls);
             return this;
         },
 
         // remove classname
         removeClass: function(cls){
-            if(typeof cls !== "string") return "Class name must be String Type";
             this[0].classList.remove(cls);
             return this;
         },
 
         // check if has a classname
         hasClass: function(cls){
-            if(typeof cls !== "string") return "Class name must be String Type";
             if(!this[0]) return false;
             return this[0].classList.contains(cls)? true : false;
         },
 
         // css
-        // Argument: String: the css property name, or Object: in key value pair
         css: function(property){
-            if(typeof property !== "string" && typeof property !== "object") return "Property can be only String or Object type";
-
             // if prop is String, then get the css property
             if(typeof property === "string"){
             return global.getComputedStyle(this[0], null).getPropertyValue(property); // Note: the pseudo element is ignored
@@ -374,12 +331,8 @@
         },
 
         // click event
-        click: function(callback, bubbling){
-            if(typeof callback !== "function") return "Paramete Type Error";
-
-            var bubbling = bubbling === undefined ? true : bubbling;
-            var callback = callback;
-
+        // Note: if bubbling is false, the click target has to be this[0], otherwise it can be any child of this[0]
+        click: function(callback, bubbling = true){
             this[0].addEventListener("click", (function(e) {
                 if(!bubbling) {
                     if(this[0] === e.target) {
@@ -389,35 +342,30 @@
                     callback.call(this);
                 }
             }).bind(this), false);
-              
         },
 
-        // if not click on this element
-        notClickOn: function(elem, callback){
-            if(typeof callback !== "function") return "Parameter is not in Function Type!";
-            if(!isObj(elem)) return "Must pass in an im Object!";
-
-            document.addEventListener("click", (function(e){
-                e.preventDefault();
-                // if HTMLCollection
-                if(elem[0].length){
-                    var inArray = [];
-                    elem.each(function(i){
-                        e.target === i[0]? inArray.push(true) : inArray.push(false);
-                    });
-                    if(!inArray.includes(true)) callback.call(this);
-                }
-                // if HTMLElement
-                else{
-                    if(e.target !== elem[0]) callback.call(this);
-                } 
-            }).bind(this), false);
-            return this;
-        },
+        // // if not click on this element
+        // notClickOn: function(elem, callback){
+        //     document.addEventListener("click", (function(e){
+        //         e.preventDefault();
+        //         // if HTMLCollection
+        //         if(elem[0].length){
+        //             var inArray = [];
+        //             elem.each(function(i){
+        //                 e.target === i[0]? inArray.push(true) : inArray.push(false);
+        //             });
+        //             if(!inArray.includes(true)) callback.call(this);
+        //         }
+        //         // if HTMLElement
+        //         else{
+        //             if(e.target !== elem[0]) callback.call(this);
+        //         } 
+        //     }).bind(this), false);
+        //     return this;
+        // },
 
         // focus event
         onfocus: function(callback) {
-            if(typeof callback !== "function") return "Parameter Type Error";
             this[0].addEventListener("focus", callback.bind(this), false);
         },
 
@@ -427,19 +375,16 @@
 
         // blur event (out of focus)
         onblur: function(callback) {
-            if(typeof callback !== "function") return "Parameter Type Error";
             this[0].addEventListener("blur", callback.bind(this), false);
         },
 
         // keydown event
         keydown: function(callback) {
-            if(typeof callback !== "function") return "Parameter Type Error";
             this[0].addEventListener("keydown", callback.bind(this), false);
         },
 
         // keyup event
         keyup: function(callback) {
-            if(typeof callback !== "function") return "Parameter Type Error";
             this[0].addEventListener("keyup", callback.bind(this), false);
         },
 
